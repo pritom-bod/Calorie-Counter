155dc585d7463d1e2b619621d95fb13dd19f1f38
pritom-bod <pritomsarker406@gmail.com>
2025-05-09 01:58:28 +0600

    set database admin

--- CaloriCounter/CaloriCounter/settings.py
+++ CaloriCounter/CaloriCounter/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_DIRS = BASE_DIR/'static'
-
+CSRF_TRUSTED_ORIGINS= ["https://calorie-counter-production-ea7b.up.railway.app"]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
